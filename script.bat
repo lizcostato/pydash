2a5221a8467f339cb8317c052d7081c20c7b93f2
lizcostato <64735695+lizcostato@users.noreply.github.com>
2021-10-20 22:35:59 -0300

    Resultado da primeira vez que rodei no ,meu pc
    Só para salvar alguns resultados no git

---
+++ script.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\Administrador\AppData\Local\Programs\Python\Python38\python.exe" "C:\Users\Administrador\Documents\UNB\setimo_semestre\Transmissao de Dados\Trabalho-git\pydash\main.py"
+pause
